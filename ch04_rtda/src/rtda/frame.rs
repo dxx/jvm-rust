d2782bcf1c8fb14c4f75bf545f457e4d3a1e58b0
dxx <dx1898@foxmail.com>
2022-03-06 21:42:39 +0800

    Chapter four

--- ch04_rtda/src/rtda/frame.rs
+++ ch04_rtda/src/rtda/frame.rs
@@ -2,7 +2,6 @@ use super::{local_vars::LocalVars, operand_stack::OperandStack};
 
 /// Stack Frame
 pub struct Frame {
-    pub lower: Option<Box<Frame>>, // Stack is implemented as linked list
     local_vars: LocalVars,
     operand_stack: OperandStack,
 }
@@ -10,14 +9,9 @@ pub struct Frame {
 impl Frame {
     pub fn new(max_locals: usize, max_size: usize) -> Self {
         Frame {
-            lower: None,
             local_vars: LocalVars::new(max_locals),
             operand_stack: OperandStack::new(max_size),
         }
-    }
-
-    pub fn set_lower(&mut self, lower: Option<Box<Frame>>) {
-        self.lower = lower;
     }
 
     pub fn get_local_vars(&mut self) -> &mut LocalVars {
